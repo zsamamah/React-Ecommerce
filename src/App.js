72acc6833aa9a9833c48ff4e01cb907605ab0feb
Rahaf-Arafeh <arafeh.rahaf@gmail.com>
2021-12-03 21:09:28 +0300

    rahaf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      hello app
+
     </div>
   );
 }
---
+++ src/Components/Shop.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Products from './Products';
+
+
+export class Shop extends Component {
+    render() {
+        return (
+            <div >
+                <div className='shop-section'>
+                    <img src='https://images.pexels.com/photos/2148217/pexels-photo-2148217.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' alt='labtop background' className='shop-bg' />
+                </div>
+                <div className='shop-container'>
+                    <Products />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Shop
