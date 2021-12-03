a82e816f842fdb1e04f3760a608c32211feabd64
zsamamah <zsamamah@yahoo.com>
2021-12-03 20:21:44 +0200

    test commit

---
+++ src/Components/Products.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import ProductItem from './ProductItem';
+
+export class Products extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: JSON.parse(localStorage.getItem('items'))
+        }
+    }
+
+    render() {
+        if (!localStorage.getItem('items')) {
+            return (<h1 className='shop-text-center'>No Products Yet!</h1>);
+        } else
+            return (
+                <div className='shop-card-grid'>
+                    {
+                        this.state.items.map(item => {
+                            return <ProductItem key={item.id} item={item} />
+                        })
+                    }
+                </div>
+            )
+    }
+}
+
+export default Products
