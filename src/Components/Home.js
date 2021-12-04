1645205237fe7350cfa83dc5b18c424fb73d9c2d
kholoud khraisat <kholoudmahmoud.kh@gmail.com>
2021-12-04 23:27:36 +0300

    kholoud push saturday

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Testimonial from './Testimonials';
 import Team from './Ourteam';
+import Intro from './Intro.js';
+import Service from './Services';
 
 export default class Home extends Component {
     render() {
         return (
             <div className="homePageContainer">
+                <Intro/>
+                <Service/>
                 {/* Intro background Khoulud  */}
                 {/* API Weather Sanad */}
                 <Team />
