a3c527d982aa3354dfd315e5c73c77e19b3cde97
zsamamah <zsamamah@yahoo.com>
2021-12-06 02:55:53 +0200

    edit css

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,28 +1,8 @@
 import React, { Component } from 'react';
-import bgImg from './accounts-assets/bg.jpg'
 import '../hero.css';
 
 export class Hero extends Component {
     render() {
-        switch(window.location.pathname){
-            case "/admin":
-                title="Admin Dashboard";
-                break;
-            case "/shop":
-                title="Shop Page"
-                break;
-            case "/account":
-                title="Account Page"
-                break;
-            case "/cart":
-                title="Cart Page";
-                break;
-            case "/checkout":
-                title="Checkout Page"
-                break;
-            default:
-                title="Page not Found !"
-        }
         return (
             <div className="hero-bg">
                 <div><p>{this.props.title}</p></div>
@@ -30,5 +10,4 @@ export class Hero extends Component {
         )
     }
 }
-let title
 export default Hero
